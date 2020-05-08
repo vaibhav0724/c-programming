c0bd2e4968190d656e0417c79cfcd2b8aacfef99
vaibhav0724 <63444851+vaibhav0724@users.noreply.github.com>
2020-05-08 22:37:53 +0530

    Size of various keywords.c

---
+++ Size of various keywords.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+Cout<<"size of char:"<<sizeof(char)<<"byte"<<endl;
+Cout<<"size of int:"<<sizeof(int)<<"bytes"<<endl;
+Cout<<"size of float:"<<sizeof(float)<<"bytes"<<endl;
+Cout<<"size of double:"<<sizeof(double)<<"bytes"<<endl;
+System ("pause");
+}

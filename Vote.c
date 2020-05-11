d3cd34fc14f0fc08f0cc561eb7749f0699e1030a
vaibhav0724 <63444851+vaibhav0724@users.noreply.github.com>
2020-05-11 23:45:20 +0530

    Vote.c

---
+++ Vote.c
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std
+int main()
+{
+int age;
+cout<<"enter age of a voter:";
+cin>>age;
+if(age>=18)
+{
+Cout<<"\n voter can give vote";
+}
+else{
+cout<<"\n you are not eligible";
+return 0;
+}
